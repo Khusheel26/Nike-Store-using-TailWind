b9e553de4db2dca2183276352ddcf0b57feb62f1
Your Name <you@example.com>
2024-01-03 12:21:21 +0530

    Initial Commit

---
+++ src/sections/index.js
@@ -0,0 +1,13 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import CustomersReviews from './CustomersReviews';
+import Services from './Services';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Subscribe from './Subscribe';
+import SpecialOffers from './SpecialOffers';
+
+
+export {
+    Hero,Footer,CustomersReviews,Services,PopularProducts,SuperQuality,Subscribe,SpecialOffers
+}
