25b7e4998eeed9ace3a1286e8b6d22a828cba599
Pratham Baliyan <104058723+PrathamBaliyan@users.noreply.github.com>
2024-08-21 11:48:22 +0530

    Create prod-output.tf

---
+++ prod-output.tf
@@ -0,0 +1,17 @@
+output "vpc_id" {
+  value = aws_vpc.production_vpc
+}
+output "vpc_cidr_block" {
+  value = aws_vpc.production_vpc.cidr_block
+}
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "frontend_security_group_id" {
+  value = aws_security_group.sg_grp.id
+}
